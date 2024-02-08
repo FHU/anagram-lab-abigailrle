e0631d03eaf250d508d1e7d602096b33c52aa8f4
Abigail <abigail.leland@students.fhu.edu>
2024-02-08 13:23:51 -0600

    main.py
    no whitespace

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def anagram(word1, word2):
-    word1 = word1.replace(' ', '')
-    word2 = word2.replace(' ', '')
-    word1 = word1.strip().lower()
-    word2 = word2.strip().lower()
-    index_number = len(word1) - 1
-    for i in range(index_number):
-        if word1[index_number] in word2:
-            index_number -= index_number
-        else:
-            return False
-    return True
+    if word1 == ' ' or word2 == ' ':
+        return False
+    else:
+        word1 = word1.replace(' ', '')
+        word2 = word2.replace(' ', '')
+        word1 = word1.strip().lower()
+        word2 = word2.strip().lower()
+        index_number = len(word1) - 1
+        for i in range(index_number):
+            if word1[index_number] in word2:
+                index_number -= index_number
+            else:
+                return False
+        return True
 #github
     
 
