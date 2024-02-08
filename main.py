217289eaa0f919d458f03fece1b5ba1607f7010c
Abigail <abigail.leland@students.fhu.edu>
2024-02-07 22:51:55 -0600

    main.py
    First draft

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(word1, word2):
+    index_number = len(word1) - 1
+    for i in range(index_number):
+        if word1[index_number] in word2:
+            index_number -= index_number
+        else:
+            return False
+    return True
+    
+
 
 if __name__ == '__main__':
     #REMOVE PASS YOUR CODE GOES HERE
-    pass
+    word_one = input().strip().lower()
+    word_two = input().strip().lower()
+    print(anagram(word_one, word_two))
     
